f98cb271fcdd1fed9270b29b9c7bea57542adfe6
Willem Thiart <himself@willemthiart.com>
2015-04-30 20:25:28 +0900

    Refactored error response

--- src/main.c
+++ src/main.c
@@ -46,10 +46,7 @@ server_t *sv = &server;
 static int __http_error(h2o_req_t *req, int status_code, const char* reason)
 {
     static h2o_generator_t generator = { NULL, NULL };
-    h2o_iovec_t body;
-
-    body.base = "";
-    body.len = 0;
+    static h2o_iovec_t body = { .base = "", .len = 0 };
     req->res.status = status_code;
     req->res.reason = reason;
     h2o_add_header(&req->pool,
